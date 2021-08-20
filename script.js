a5b901008404bebe04a11f3e89d8e2281f0fd59b
Vivek-jpg <kumarvivekazio@gmail.com>
2021-08-20 18:04:01 +0530

    edit

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-6d96dbda29b0fcf4e3659ed6dcb6060a
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
 
-api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+const weatherApi = {
+    key:"6d96dbda29b0fcf4e3659ed6dcb6060a"
+    baseUrl: "https://api.openweathermap.org/data/2.5/weather?"
+}
+
+const searchInputBox = document.getElementById('input-box');
+searchInputBox.addEventListener('keypress',(Event) => {
+    console.log(searchInputBox);
+
+});
+
+
+
+
+
